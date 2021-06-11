95b13e65a619a0138045934e35c19f20c95ccce4
Mateus Ferreira <mateusfbsoares@gmail.com>
2021-06-11 11:21:37 -0300

    edit kernel.asm demo x86 program

--- QEMU files/kernel.asm
+++ QEMU files/kernel.asm
@@ -7,6 +7,10 @@ print_hi:
 	int 10h
 	mov al, 'i'
 	int 10h
+	mov al, ' '
+	int 10h
+
+
 	ret
 
 start:
@@ -27,32 +31,45 @@ start:
 	mov al, 'a'
 	int 10h
 
+	mov al, ' '
+	int 10h
+
 
 	; push and pop
 	push 'A'
 	push 'B'
 	push 'C'
+	push 'D'
 	pop ax
 	pop bx
 	pop cx
+	pop dx
 
 	; push and add
 	push 1
 	push 2
-	add sp, 2
-	add sp, 2
+	push 3
+	add sp, 6
 
 
 	; call function
 	call print_hi
+
+	; input
+	mov ah, 00h		; (argumento do int 16h)
+	int 16h			; http://www.ctyme.com/intr/rb-1754.htm --> guarda o caractere ASCII em al
+	mov ah, 0eh		; 0eh = 14 em hexadecimal --> modo de impressão do int 10h
+	int 10h
+
+	; char to int
+	sub ax, 48
 
 	; ???
 	mov ax, 'A' 
 	mov bx, 10
 	mov bx, 10
 
-	; int 16h
-	mov al, 'H'
+	jmp end
 
 
 end:
